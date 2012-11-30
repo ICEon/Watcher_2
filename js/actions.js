e001b22b2cb6b2cfe84bef43daa5cddf330efa65
Eon Matrix <matrix_eon@yahoo.com>
2012-11-29 20:58:44 -0600

    Prueba_1

---
+++ js/actions.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+function loginConn(nick,pass){
+
+	datos = "name="+nick+"&password="+pass;
+	$.ajax({
+		type: "POST",
+		url: "http://192.168.1.68/watcher/login.php",
+		data: datos
+	}).done(function( msg ) {
+		alert(msg);
+	});
+}
+
+$(document).ready(function(e) {
+	$('.Send').tap(function(){
+		var formulario = $(this).parents('form');
+		switch(formulario.attr('name')){
+			case 'login':
+				var nick = formulario.children('input:eq(0)').val();
+				var pass = formulario.children('input:eq(1)').val();
+				
+				loginConn(nick,pass);
+				
+				break;	
+		}
+	});
+    document.addEventListener("deviceready", function(){
+		
+	});
+});
