8076e3f6943a0a37625649ac99992bf23cea625a
Eon Matrix <matrix_eon@yahoo.com>
2012-12-01 03:13:20 -0600

    Prueba_17

--- js/actions.js
+++ js/actions.js
@@ -72,7 +72,7 @@ var Datos = JSON.parse (msg);
 
 alert (inspeccionar (Datos));
 
-
+document.write(Datos.Nombre);
 $('#devic table td').eq(1).text(Datos.Nombre.value());
 $('#devic table td').eq(3).text(Datos.Nombre);
 $('#devic table td').eq(5).text();
